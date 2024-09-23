c7362ab0f8ce1de0abb1bc5912bf36f9ee638c32
alfred <aprenderenlinea3@gmail.com>
2024-09-23 02:17:21 +0000

    subir a github

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "¡Hola, CI/CD!"
+    return "¡Hola, CI/CD! nueva modificacion"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
